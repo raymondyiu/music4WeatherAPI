e131975e4eabf40ce43b0a05b3e65413aaa24c8d
Raymond <raymond.yiu@gmail.com>
2023-03-20 22:17:10 +0000

    init project - Music for Weather API

---
+++ src/main/java/com/techreturners/music4WeatherAPI/Music4WeatherApiApplication.java
@@ -0,0 +1,13 @@
+package com.techreturners.music4WeatherAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Music4WeatherApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Music4WeatherApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/techreturners/music4WeatherAPI/Music4WeatherApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techreturners.music4WeatherAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Music4WeatherApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
