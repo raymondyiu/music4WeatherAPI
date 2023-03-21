8462c7d7c237184e59eb0a3231f20cd48e77bc80
MRastogi5 <118384562+MRastogi5@users.noreply.github.com>
2023-03-21 23:16:26 +0000

    Added : get getTracksByQueryParam service

--- src/main/java/com/techreturners/music4WeatherAPI/controller/DeezerController.java
+++ src/main/java/com/techreturners/music4WeatherAPI/controller/DeezerController.java
@@ -1,16 +1,14 @@
 package com.techreturners.music4WeatherAPI.controller;
 
 import com.techreturners.music4WeatherAPI.model.Track;
+import com.techreturners.music4WeatherAPI.model.TrackList;
 import com.techreturners.music4WeatherAPI.service.DeezerAPICallService;
-import com.techreturners.music4WeatherAPI.service.Music4WeatherService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class DeezerController {
@@ -20,15 +18,23 @@ public class DeezerController {
 
     @RequestMapping("/")
     @ResponseBody
-    private String hello() {
+    public String hello() {
         return "Hello !!!!";
     }
 
-
+    //https://api.deezer.com/track/3135556
     @RequestMapping("/track/{id}")
     @ResponseBody
-    private ResponseEntity<Track>  getTrackById(@PathVariable Long id) {  // http://localhost:8080/track/3135556
+    public ResponseEntity<Track>  getTrackById(@PathVariable Long id) {  // http://localhost:8080/track/3135556
         Track track = deezerAPICallService.getTrackById(id);
          return new ResponseEntity<>(track, HttpStatus.OK);
+    }
+
+    //https://api.deezer.com/search?q=eminem
+    @RequestMapping("/search")
+    @ResponseBody
+    public ResponseEntity<List<Track>>  getTracksByQueryParam(@RequestParam("q") String q) {  // http://localhost:8080/search?q=eminem
+        List<Track> trackList = deezerAPICallService.getTracksByParam(q);
+        return new ResponseEntity<>(trackList, HttpStatus.OK);
     }
 }
--- src/main/java/com/techreturners/music4WeatherAPI/service/DeezerAPICallService.java
+++ src/main/java/com/techreturners/music4WeatherAPI/service/DeezerAPICallService.java
@@ -1,8 +1,13 @@
 package com.techreturners.music4WeatherAPI.service;
 
 import com.techreturners.music4WeatherAPI.model.Track;
+import com.techreturners.music4WeatherAPI.model.TrackList;
+
+import java.util.List;
 
 public interface DeezerAPICallService {
 
     Track getTrackById(Long id);
+
+    List<Track> getTracksByParam(String param);
 }
