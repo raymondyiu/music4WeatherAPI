afbb058e3b987c9cd1c838e3d5d33581f85601ef
MRastogi5 <118384562+MRastogi5@users.noreply.github.com>
2023-03-21 23:15:48 +0000

    Added : getTracksByQueryParam service

---
+++ src/test/java/com/techreturners/music4WeatherAPI/controller/DeezerControllerTest.java
@@ -0,0 +1,11 @@
+package com.techreturners.music4WeatherAPI.controller;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@AutoConfigureMockMvc
+@SpringBootTest
+public class DeezerControllerTest {
+
+
+}
