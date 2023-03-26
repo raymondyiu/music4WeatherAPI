5fb21ab86d9078fae7c471265bf808afde82b221
ChrixWX92 <chris@wards-net.co.uk>
2023-03-26 18:14:58 +0100

    feat: Weather details display + window resizing

--- src/main/java/com/techreturners/music4WeatherAPI/view/Music4WeatherView.java
+++ src/main/java/com/techreturners/music4WeatherAPI/view/Music4WeatherView.java
@@ -1,140 +1,96 @@
 package com.techreturners.music4WeatherAPI.view;
 
 import com.techreturners.music4WeatherAPI.controller.Music4WeatherController;
+import com.techreturners.music4WeatherAPI.controller.WeatherController;
 import com.techreturners.music4WeatherAPI.model.Track;
+import com.techreturners.music4WeatherAPI.model.Weather;
+import javazoom.jl.player.Player;
+import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
 
 @Component
 public class Music4WeatherView extends JFrame {
 
     @Autowired
     private Music4WeatherController music4WeatherController;
+    @Autowired
+    private WeatherController weatherController;
 
-    public Music4WeatherView() throws Exception {
-//
-//        String userCityInput;
-//
-//        JFrame frame = initUI();
-//        frame.setVisible(true);
-//
-//        // TODO: Happens on button press
-//        Track track = music4WeatherController.getTrack("Oxford").getBody();
-
+    public Music4WeatherView() {
 
         initUI();
 
     }
 
-//    private UI initUI() {
-//
-//        UI ui = new UI();
-//        ui.setTitle("Generate Plateau");
-//        ui.setLocationRelativeTo(null);
-//        ui.setVisible(true);
-//        ui.setBounds(600, 250, 600, 470);
-//        ui.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
-//        ui.setResizable(false);
-//
-//        ui.addWindowListener(new java.awt.event.WindowAdapter() {
-//            @Override
-//            public void windowClosing(java.awt.event.WindowEvent windowEvent) {
-//                if (JOptionPane.showConfirmDialog(
-//                        ui, "Proceed with terminal input?", "Terminal Input",
-//                        JOptionPane.YES_NO_OPTION,
-//                        JOptionPane.QUESTION_MESSAGE) == JOptionPane.YES_OPTION) {
-//
-//                    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-//                    int[] coords;
-//                    int[] roverCoords;
-//                    while (true) {
-//                        try {
-//                            coords = inputCoords(reader);
-//                            break;
-//                        } catch (NumberFormatException | IOException | ArrayIndexOutOfBoundsException e) {
-//                            System.out.println("\nERROR: Invalid format, please try again.\n");
-//                        }
-//                    }
-//                    while (true) {
-//                        try {
-//                            roverCoords = inputRoverCoords(reader);
-//                            break;
-//                        } catch (NumberFormatException | IOException | ArrayIndexOutOfBoundsException e) {
-//                            System.out.println("\nERROR: Invalid format, please try again.\n");
-//                        }
-//                    }
-//                    ui.dispose();
-//                    int[] finalCoords = coords;
-//                    int[] finalRoverCoords = roverCoords;
-//                }
-//                else {
-//                    System.exit(0);
-//                }
-//            }
-//
-//            private int[] inputCoords(BufferedReader reader) throws IOException {
-//                System.out.println("Please specify your plateau size in coordinates separated by a space.");
-//                System.out.println("(e.g. for an 8x5 grid: \"7 4\"):");
-//                String[] args = reader.readLine().split(" ");
-//                return new int[]{Integer.parseInt(args[0]), Integer.parseInt(args[1])};
-//            }
-//            private int[] inputRoverCoords(BufferedReader reader) throws IOException {
-//                System.out.println("\nPlease specify your initial rover's starting coordinates and facing");
-//                System.out.println("in the following format:  \"x y <N/E/S/W>\"");
-//                System.out.println("(e.g. \"3 1 S\"):");
-//                String[] args = reader.readLine().split(" ");
-//                return new int[]{Integer.parseInt(args[0]), Integer.parseInt(args[1]), args[2].charAt(0)};
-//            }
-//
-//        });
-//
-//        return ui;
-//
-//    }
-
-    private void initUI() throws Exception {
-
-//        var quitButton = new JButton("Quit");
-//
-//        quitButton.addActionListener((ActionEvent event) -> {
-//
-//            System.exit(0);
-//        });
-//
-//        createLayout(quitButton);
-//
-//        setTitle("Quit button");
-//        setSize(300, 200);
-//        setLocationRelativeTo(null);
-//        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    private void initUI() {
 
         UI ui = new UI();
+
+        ui.button.addActionListener(new ActionListener() {
+            @SneakyThrows
+            public void actionPerformed(ActionEvent e) {
+                ResponseEntity<Track> trackQuery = music4WeatherController.getTrack(ui.cityBox.getText());
+
+                ResponseEntity<Weather> weatherQuery = weatherController.getWeatherDetails(ui.cityBox.getText());
+//                if (trackQuery.getStatusCode() == 400)
+
+                ui.trackLabel.setText(trackQuery.getBody().getTitle());
+
+                URL weatherIconURL = new URL("https:" + weatherQuery.getBody().getCurrent().getCondition().getIcon());
+                BufferedImage weatherImage = ImageIO.read(weatherIconURL);
+                ImageIcon weatherIcon = new ImageIcon(weatherImage);
+
+                URL artistURL = new URL(trackQuery.getBody().getArtist().getPicture());
+                BufferedImage artistImage = ImageIO.read(artistURL);
+                ImageIcon artistIcon = new ImageIcon(artistImage);
+
+                URL albumURL = new URL(trackQuery.getBody().getAlbum().getCover_small());
+                BufferedImage albumImage = ImageIO.read(albumURL);
+                ImageIcon albumIcon = new ImageIcon(albumImage);
+
+                ui.location.setText(weatherQuery.getBody().getLocation().getName() + ", " + weatherQuery.getBody().getLocation().getCountry());
+                ui.weatherIcon.setIcon(weatherIcon);
+                ui.temperature.setText(weatherQuery.getBody().getCurrent().getTemp_c() + "°c");
+                ui.otherWeatherLabel.setText(
+                        "<html>Precipitation: " + weatherQuery.getBody().getCurrent().getPrecip_mm() +
+                        "mm<br/>Humidity: " + weatherQuery.getBody().getCurrent().getHumidity() +
+                        "%<br/>Wind: " + weatherQuery.getBody().getCurrent().getWind_mph() + "mph</html>");
+
+                ui.artistLabel.setText("Artist: " + trackQuery.getBody().getArtist().getName());
+                ui.artistIcon.setIcon(artistIcon);
+                ui.albumLabel.setText("Album: " + trackQuery.getBody().getAlbum().getTitle());
+                ui.albumIcon.setIcon(albumIcon);
+
+                new Thread(() -> {
+                    try { playSample(trackQuery.getBody().getPreview()); } catch (Exception exception) { exception.printStackTrace(); }
+                }).start();
+
+            }
+        });
 
         ui.setVisible(true);
 
     }
 
-    private void createLayout(JComponent... arg) {
+    private void playSample(String urlString) throws Exception {
 
-        var pane = getContentPane();
-        var gl = new GroupLayout(pane);
-        pane.setLayout(gl);
+        URLConnection conn = new URL(urlString).openConnection();
+        InputStream is = conn.getInputStream();
 
-        gl.setAutoCreateContainerGaps(true);
+        Player playMP3 = new Player(is);
+        playMP3.play();
 
-        gl.setHorizontalGroup(gl.createSequentialGroup()
-                .addComponent(arg[0])
-        );
-
-        gl.setVerticalGroup(gl.createSequentialGroup()
-                .addComponent(arg[0])
-        );
     }
-
 }
+
--- src/main/java/com/techreturners/music4WeatherAPI/view/UI.java
+++ src/main/java/com/techreturners/music4WeatherAPI/view/UI.java
@@ -3,81 +3,77 @@ package com.techreturners.music4WeatherAPI.view;
 import javax.swing.*;
 
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
-public class UI extends JFrame implements ActionListener {
+public class UI extends JFrame {
 
     JLabel intro;
-    JLabel prompt, xLabel, zLabel, xLabel2, zLabel2, fLabel;
-    JTextField xBox, zBox, xBox2, zBox2;
+    JLabel trackLabel, cityLabel, artistLabel, artistIcon, albumLabel, albumIcon, location, weatherIcon, temperature, otherWeatherLabel;
+    JTextField cityBox;
     JButton button;
     Container container;
 
     public UI() {
-        intro = new JLabel("Track title"/*track.getTitle()*/);
-        intro.setFont(new Font("Courier", Font.BOLD, 20));
-        prompt = new JLabel(".");
-        xLabel = new JLabel(":");
-        xBox = new JTextField();
-        zLabel = new JLabel(":");
-        zBox = new JTextField();
-        xLabel2 = new JLabel(":");
-        xBox2 = new JTextField();
-        zLabel2 = new JLabel(":");
-        zBox2 = new JTextField();
-        zLabel2 = new JLabel(":");
-        zBox2 = new JTextField();
+        intro = new JLabel("Music4Weather"/*track.getTitle()*/);
+        intro.setFont(new Font("Agency FB", Font.BOLD, 40));
+        intro.setForeground(Color.LIGHT_GRAY);
+        location = new JLabel("");
+        location.setForeground(Color.LIGHT_GRAY);
+        location.setFont(new Font("Bahnschrift", Font.PLAIN, 18));
+        weatherIcon = new JLabel("");
+        temperature = new JLabel("");
+        temperature.setForeground(Color.LIGHT_GRAY);
+        temperature.setFont(new Font("Bahnschrift", Font.PLAIN, 25));
+        otherWeatherLabel = new JLabel("");
+        otherWeatherLabel.setFont(new Font("Bahnschrift", Font.PLAIN, 14));
+        otherWeatherLabel.setForeground(Color.LIGHT_GRAY);
+        trackLabel = new JLabel("");
+        cityLabel = new JLabel("Get track by city:");
+        cityLabel.setForeground(Color.LIGHT_GRAY);
+        cityLabel.setFont(new Font("Bahnschrift", Font.PLAIN, 18));
+        cityBox = new JTextField();
+        artistLabel = new JLabel();
+        artistIcon = new JLabel();
+        albumLabel = new JLabel();
+        albumIcon = new JLabel(":");
         button = new JButton("Get Track");
         container = getContentPane();
         container.setLayout(null);
         setBounds();
         addComponents();
-        addActionListener();
+        this.setBounds(0, 0, 800, 520);
+        this.container.setBackground(Color.DARK_GRAY);
     }
 
     public void setBounds() {
-        intro.setBounds(85, 10, 600, 30);
-        prompt.setBounds(112, 60, 600, 30);
-        xLabel.setBounds(120, 110, 100, 30);
-        xBox.setBounds(200, 110, 250, 30);
-        zLabel.setBounds(120, 160, 100, 30);
-        zBox.setBounds(200, 160, 250, 30);
-        xLabel2.setBounds(120, 210, 100, 30);
-        xBox2.setBounds(200, 210, 250, 30);
-        zLabel2.setBounds(120, 260, 100, 30);
-        zBox2.setBounds(200, 260, 250, 30);
-        button.setBounds(165, 370, 250, 30);
+        intro.setBounds(85, 50, 600, 30);
+        location.setBounds(340, 30, 600, 30);
+        weatherIcon.setBounds(340, 55, 64, 64);
+        temperature.setBounds(415, 63, 128, 64);
+        otherWeatherLabel.setBounds(490, 20, 192, 128);
+        trackLabel.setBounds(112, 80, 600, 30);
+        cityLabel.setBounds(100, 130, 200, 30);
+        cityBox.setBounds(260, 130, 250, 30);
+        artistLabel.setBounds(120, 180, 100, 30);
+        artistIcon.setBounds(120, 230, 300, 300);
+        albumLabel.setBounds(120, 280, 100, 30);
+        albumIcon.setBounds(150, 380, 112, 112);
+        button.setBounds(165, 390, 250, 30);
     }
 
     public void addComponents() {
         container.add(intro);
-        container.add(prompt);
-        container.add(xLabel);
-        container.add(xBox);
-        container.add(zLabel);
-        container.add(zBox);
-        container.add(xLabel2);
-        container.add(xBox2);
-        container.add(zLabel2);
-        container.add(zBox2);
+        container.add(location);
+        container.add(weatherIcon);
+        container.add(temperature);
+        container.add(otherWeatherLabel);
+        container.add(trackLabel);
+        container.add(cityLabel);
+        container.add(cityBox);
+        container.add(artistLabel);
+        container.add(artistIcon);
+        container.add(albumLabel);
+        container.add(albumIcon);
         container.add(button);
-    }
-
-    public void addActionListener() {
-        button.addActionListener(this);
-    }
-
-    @Override
-    public void actionPerformed(ActionEvent e) {
-
-        if (e.getSource() == button) {
-
-            // TODO: This should be moved to the View Controller to fetch the track on button press
-
-            this.dispose();
-        }
-
     }
 
 }
